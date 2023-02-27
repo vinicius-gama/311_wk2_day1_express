7785a07c27ac0b579ff511fd676aa786ccb3f58a
vinicius-gama <almeidagama97@gmail.com>
2023-02-26 22:11:47 -0600

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = process.env.PORT || 4000
 
-const { users } = require('./state')
+const { users } = require("./state")
 
 /* BEGIN - create routes here */
+// basic routing
 
+// app.get('/', function ( req, res)) {
+// fetch from database
+// load pages
+// return json
+// full access to request e response
+
+app.use((req, res, next) => {
+	next()
+})
+
+app.get("/users", (req, res) => {
+	res.json(users)
+})
+
+app.get("/users/:id", (req, res) => {})
+
+app.post()
+app.put()
+app.delete()
 
 /* END - create routes here */
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
